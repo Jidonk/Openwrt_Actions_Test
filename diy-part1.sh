8033900ed56d6fe1a0441074375382bc779010c3
Jidonk <1576324301@qq.com>
2023-09-19 09:17:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-git clone https://github.com/animegasan/luci-app-wolplus.git package/wolplus
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#git clone https://github.com/animegasan/luci-app-wolplus.git package/wolplus
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
